9f5171d7261aba2fe09a15c5102f5a5d1bb00c10
ScottDenton <scottyd_14@hotmail.com>
2019-03-18 11:38:45 -0700

    Done.

--- main.js
+++ main.js
@@ -1,10 +1,37 @@
 // Defining text characters for the empty and full hearts for you to use later.
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
+// hides error field when page loaded
+const modal = document.getElementById('modal')
+const modalMessage = document.getElementById('modal-message')
+modal.classList.add('hidden')
+//find all hearts and add event listener to invoke mimicServercall
+const allHearts = document.querySelectorAll('.like-glyph')
 
-// Your JavaScript code goes here!
+allHearts.forEach((heart) => heart.addEventListener('click',
+addNewLike))
 
-
+function addNewLike(e){
+  mimicServerCall()
+  .then(data => {
+    toggleHeart(e)
+  })
+  .catch(error => {
+    modalMessage.textContent = error
+    modal.classList.remove('hidden')
+    setTimeout(()=> {modal.classList.add('hidden')}, 5000)
+  })
+}
+// toggles the heart class visibility
+function toggleHeart(e){
+  if (e.target.textContent.includes(FULL_HEART)){
+    e.target.textContent = EMPTY_HEART
+    e.target.classList.remove('activated-heart')
+  } else {
+    e.target.textContent = FULL_HEART
+    e.target.classList.add("activated-heart")
+  }
+}
 
 
 //------------------------------------------------------------------------------
